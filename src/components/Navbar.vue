bed880bc8c1374af2f6be69562563feaf68a49d2
Vezil <SzymonWojaczek@wp.pl>
2020-08-23 00:50:37 +0200

    add scrolled menu functionality

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -32,6 +32,26 @@
 export default {
     data() {
         return {};
+    },
+
+    mounted() {
+        this.initMenuScrolled();
+    },
+
+    methods: {
+        initMenuScrolled() {
+            const header = document.querySelector('.main-header');
+
+            window.addEventListener('scroll', () => {
+                const scrollPosition = window.scrollY;
+
+                if (scrollPosition > 10) {
+                    header.classList.add('scrolled');
+                } else {
+                    header.classList.remove('scrolled');
+                }
+            });
+        }
     }
 };
 </script>
