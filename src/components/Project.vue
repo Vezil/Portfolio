940c357a19367a07b0b7011c1a5d617b09f288e2
Vezil <SzymonWojaczek@wp.pl>
2020-09-27 20:10:14 +0200

    add more cleaning in code

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,107 +1,40 @@
 <template>
-    <div class="container">
+    <div class="card">
         <div class="box">
-            <div class="content">
-                <h2>YERBA PROJECT</h2>
-                <p> This project is fine </p>
+            <div class="imgBox">
+                <img :src="require(`@/assets/images/${logoLink}`)" alt="link" />
+            </div>
+            <div class="contentBox">
+                <div>
+                    <h2>{{ title }}</h2>
+                    <p>
+                        {{ content }}
+                    </p>
+                </div>
             </div>
         </div>
-        <svg>
-            <filter id="wavy">
-                <feTurbulence
-                    x="0"
-                    y="0"
-                    baseFrequency="0.02"
-                    numOctaves="5"
-                    seed="2"
-                >
-                    <animate
-                        attributeName="baseFrequency"
-                        dur="60s"
-                        values="0.02;0.05;0.02"
-                        repeatCount="indefinite"
-                    ></animate>
-                </feTurbulence>
-
-                <feDisplacementMap in="SourceGraphic" scale="30" />
-            </filter>
-        </svg>
     </div>
 </template>
 
 <script>
 export default {
-    methods: {}
+    props: {
+        logoLink: {
+            type: String,
+            required: true,
+            default: ''
+        },
+        title: {
+            type: String,
+            required: true,
+            default: ''
+        },
+        content: {
+            type: String,
+            required: true,
+            default: ''
+        }
+    }
 };
 </script>
-<style>
-.container {
-    position: relative;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: black;
-}
 
-.v-application--wrap {
-    background-color: black;
-}
-
-.container .box {
-    position: relative;
-    width: 300px;
-    height: 400px;
-    color: #fff;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    -webkit-box-reflect: below 15px
-        linear-gradient(transparent, transparent, rgba(0, 0, 0, 0.2));
-}
-
-.container .box:before {
-    content: '';
-    position: absolute;
-    top: -10px;
-    left: -10px;
-    width: 100%;
-    height: 100%;
-    border: 4px solid #fff;
-    background: #000;
-    filter: url(#wavy);
-}
-
-.container .box:hover {
-    color: #000;
-}
-
-.container .box:hover:before {
-    background: url('../assets/images/project1.png') no-repeat bottom center;
-    background-size: cover;
-    transition: 0.5s;
-}
-
-.container .box .content {
-    position: absolute;
-    padding: 20px;
-    text-align: center;
-    transition: 0.5s;
-}
-
-.container .box:after {
-    content: '';
-    position: absolute;
-    top: -10px;
-    left: -10px;
-    width: 50%;
-    height: 100%;
-    border: 4px solid #fff;
-    border-right: none;
-    filter: url(#wavy);
-    background: rgba(255, 255, 255, 0.1);
-}
-svg {
-    width: 0;
-    height: 0;
-}
-</style>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,221 +1,39 @@
 <template>
     <div class="projectsContainer">
         <div class="projectsSection">
-            <!-- <h1>Projects</h1> -->
-            <div class="card">
-                <div class="box">
-                    <div class="imgBox">
-                        <img src="../assets/images/project1.png" alt="" />
-                    </div>
-                    <div class="contentBox">
-                        <div>
-                            <h2>Title</h2>
-                            <p
-                                >Lorem ipsum dolor sit amet consectetur
-                                adipisicing elit. Asperiores optio quas labore
-                                vitae eaque dolorem su nt quasi eos doloremque
-                                ea ani mi molestias perspiciatis nam exp licabo
-                                blanditiis, quo, voluptate e xercitationem hic!
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="card">
-                <div class="box">
-                    <div class="imgBox">
-                        <img src="../assets/images/project1.png" alt="" />
-                    </div>
-                    <div class="contentBox">
-                        <div>
-                            <h2>Title2</h2>
-                            <p
-                                >Lorem ipsum dolor sit amet consectetur
-                                adipisicing elit. Asperiores optio quas labore
-                                vitae eaque dolorem su nt quasi eos doloremque
-                                ea ani mi molestias perspiciatis nam exp licabo
-                                blanditiis, quo, voluptate e xercitationem hic!
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="card">
-                <div class="box">
-                    <div class="imgBox">
-                        <img src="../assets/images/project1.png" alt="" />
-                    </div>
-                    <div class="contentBox">
-                        <div>
-                            <h2>Title3</h2>
-                            <p
-                                >Lorem ipsum dolor sit amet consectetur
-                                adipisicing elit. Asperiores optio quas labore
-                                vitae eaque dolorem su nt quasi eos doloremque
-                                ea ani mi molestias perspiciatis nam exp licabo
-                                blanditiis, quo, voluptate e xercitationem hic!
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="card">
-                <div class="box">
-                    <div class="imgBox">
-                        <img src="../assets/images/project1.png" alt="" />
-                    </div>
-                    <div class="contentBox">
-                        <div>
-                            <h2>Title4</h2>
-                            <p
-                                >Lorem ipsum dolor sit amet consectetur
-                                adipisicing elit. Asperiores optio quas labore
-                                vitae eaque dolorem su nt quasi eos doloremque
-                                ea ani mi molestias perspiciatis nam exp licabo
-                                blanditiis, quo, voluptate e xercitationem hic!
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="card">
-                <div class="box">
-                    <div class="imgBox">
-                        <img src="../assets/images/project1.png" alt="" />
-                    </div>
-                    <div class="contentBox">
-                        <div>
-                            <h2>Title5</h2>
-                            <p
-                                >Lorem ipsum dolor sit amet consectetur
-                                adipisicing elit. Asperiores optio quas labore
-                                vitae eaque dolorem su nt quasi eos doloremque
-                                ea ani mi molestias perspiciatis nam exp licabo
-                                blanditiis, quo, voluptate e xercitationem hic!
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="card">
-                <div class="box">
-                    <div class="imgBox">
-                        <img src="../assets/images/project1.png" alt="" />
-                    </div>
-                    <div class="contentBox">
-                        <div>
-                            <h2>Title6</h2>
-                            <p
-                                >Lorem ipsum dolor sit amet consectetur
-                                adipisicing elit. Asperiores optio quas labore
-                                vitae eaque dolorem su nt quasi eos doloremque
-                                ea ani mi molestias perspiciatis nam exp licabo
-                                blanditiis, quo, voluptate e xercitationem hic!
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Project
+                :logoLink="'store.png'"
+                :title="'Store'"
+                :content="'Store Content'"
+            />
+            <Project
+                :logoLink="'chat.png'"
+                :title="'Chat'"
+                :content="'Chat Content'"
+            />
+            <Project
+                :logoLink="'disney.png'"
+                :title="'Disney'"
+                :content="'Disney Content'"
+            />
+            <Project
+                :logoLink="'platformPig.png'"
+                :title="'Platform Pig'"
+                :content="'Platform Pig Content'"
+            />
         </div>
     </div>
 </template>
 <script>
+import Project from './Project';
+
 export default {
+    components: {
+        Project
+    },
+
     data() {
         return {};
     }
 };
 </script>
-
-<style lang="scss" scoped>
-.projectsContainer {
-    background-color: #000000;
-    background-image: linear-gradient(147deg, #000000 0%, #04619f 74%);
-    border-top-right-radius: 50px;
-    border-width: 3px;
-    border-top-style: groove;
-    border-top-color: black;
-}
-.projectsSection {
-    box-sizing: border-box;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    min-height: 100vh;
-    flex-wrap: wrap;
-    transform-style: preserve-3d;
-}
-
-.projectsSection .card {
-    position: relative;
-    width: 320px;
-    height: 320px;
-    margin: 20px;
-    transform-style: preserve-3d;
-    perspective: 1000px;
-}
-
-.projectsSection .card .box {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    transform-style: preserve-3d;
-    transition: 1s ease;
-}
-
-.projectsSection .card:hover .box {
-    transform: rotateY(180deg);
-}
-
-.projectsSection .card .box .imgBox {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-}
-
-.projectsSection .card .box .imgBox img {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-}
-
-.projectsSection .card .box .contentBox {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: #333;
-    backface-visibility: hidden;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    transform-style: preserve-3d;
-    transform: rotateY(180deg);
-}
-
-.projectsSection .card .box .contentBox div {
-    transform-style: preserve-3d;
-    padding: 20px;
-    background: linear-gradient(45deg, #fe0061, #ffeb3b);
-    transform: translateZ(100px);
-}
-
-.projectsSection .card .box .contentBox div h2 {
-    color: #fff;
-    font-size: 20px;
-    letter-spacing: 1px;
-}
-
-.projectsSection .card .box .contentBox div p {
-    color: #fff;
-    font-size: 16px;
-}
-</style>
