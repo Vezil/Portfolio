4fa7a6281d147152ccd921d65da539b7c7822c3f
Vezil <SzymonWojaczek@wp.pl>
2020-10-01 21:43:21 +0200

    add description for all projects

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -5,14 +5,14 @@
             <Project
                 :logoLink="'weloan.png'"
                 :title="'Weloan'"
-                :content="'Weloan Content'"
+                :content="weloanContent"
                 :projectLink="'https://weloan-app.prpl.io'"
             />
 
             <Project
                 :logoLink="'israelbody.png'"
                 :title="'Israelbody'"
-                :content="'Israelbody Content'"
+                :content="israelbodyContent"
                 :projectLink="'https://www.israelbody.org'"
             />
         </div>
@@ -21,19 +21,19 @@
             <Project
                 :logoLink="'store.png'"
                 :title="'Store'"
-                :content="'Store Content'"
+                :content="storeContent"
                 :projectLink="'https://vezil-store.netlify.app'"
             />
             <Project
                 :logoLink="'chat.png'"
                 :title="'Chat'"
-                :content="'Chat Content'"
+                :content="chatContent"
                 :projectLink="'https://vezil-nodechat.netlify.app'"
             />
             <Project
                 :logoLink="'disney.png'"
                 :title="'Disney'"
-                :content="'Disney Content'"
+                :content="disneyContent"
                 :projectLink="'https://vezil-disneyplus.netlify.app'"
             />
             <Project
@@ -55,6 +55,27 @@ export default {
 
     data() {
         return {
+            weloanContent: `<br/>Project created mainly in Nuxt/Vue/Node(express).
+            This website is using for taking loans (private or business)<br/><br/>
+             <span style="cursor:no-drop; color:gray; padding:5px;">Source Code</span>`,
+
+            israelbodyContent: `<br/>Project created mainly in Nuxt/Vue/Node(express).There is also
+             Elasticsearch for searching content and Tranzilla system for payment.
+              In this website we can find a large store with many products and a system of coupons and discounts,
+               food recipes and many others things...<br/><br/>
+               <span style="cursor:no-drop; color:gray; padding:5px;">Source Code</span>
+            `,
+
+            storeContent: `<br/>Small shop created in react.js .We can make payment via Paypal<br/><br/>
+            <a style="text-decoration:none; color:gold; padding:5px;" href="https://github.com/Vezil/Store" target="_blank">Source Code</a>`,
+
+            chatContent: `<br/>This chat was created in react.js and node.js + socket.io.
+             You can find here some funcionality like emojify.<br/><br/>
+               <a style="text-decoration:none; color:gold; padding:5px;" href="https://github.com/Vezil/NodeChat" target="_blank">Source Code</a>`,
+
+            disneyContent: `<br/>This project is reproduce disney plus website (created in vue.js)<br/><br/>
+            <a style="text-decoration:none; color:gold; padding:5px;" href="https://github.com/Vezil/disneyplus" target="_blank">Source Code</a>`,
+
             platformPigContent: `<br/>Experimental Platform Game created in Javascript.<br/><br/> 
                  <a style="text-decoration:none; color:gold; padding:5px;" href="https://github.com/Vezil/PlatformPig" target="_blank">Source Code</a>
                 `
