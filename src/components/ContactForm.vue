643230c5f3fedcd0a8b0de24c927da2a127575cf
Vezil <44679435+Vezil@users.noreply.github.com>
2020-11-04 21:29:05 +0100

    Feature/add frame and improve form (#8)
    * add frame
    
    * form improvements
    
    * refactor projects
    
    * add some icons to contact form
    
    * some style improvements for contact form

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -8,19 +8,33 @@
         <section class="about">
             <div class="contentBox">
                 <div class="form">
-                    <div class="inputBox">
-                        <input type="text" name="" placeholder="Full Name" />
-                    </div>
-                    <div class="inputBox">
-                        <input type="text" name="" placeholder="Email Adress" />
-                    </div>
-                    <div class="inputBox">
+                    <label class="formGroup">
+                        <input type="text" class="formControl" required />
+                        <span>
+                            <i class="fa fa-user-circle"></i> Your Name
+                        </span>
+                        <span class="border"></span>
+                    </label>
+                    <label class="formGroup">
+                        <input type="email" class="formControl" required />
+                        <span for="">
+                            <i class="fa fa-envelope-open-o"> Your Mail </i>
+                        </span>
+                        <span class="border"></span>
+                    </label>
+                    <label class="formGroup">
                         <textarea
-                            type="text"
                             name=""
-                            placeholder="Write your message here"
+                            id=""
+                            class="formControl"
+                            required
                         ></textarea>
-                    </div>
+                        <span for="">
+                            <i class="fa fa-quora"> Your Message </i>
+                        </span>
+                        <span class="border"></span>
+                    </label>
+
                     <div class="inputBox sendButton">
                         <a class="sendButtonHref" href="#">
                             <span></span>
@@ -39,161 +53,268 @@
 <script>
 export default {};
 </script>
-<style>
+<style lang="scss">
 .contactContainer {
     background: #050b0f;
     text-align: center;
     color: #fff !important;
     background: url('../assets/images/contact.png');
-}
+    height: 80vh;
+    padding: 50px;
 
-.contactContainer h2 {
-    padding: 20px;
-    font-size: 2rem;
-}
+    .contact {
+        position: relative;
+        margin-top: 5%;
+        right: 20%;
 
-.contactContainer .contact {
-    padding: 30px;
-    font-size: 1rem;
-}
+        h2 {
+            font-size: 5rem;
 
-.contentBox {
-    background-size: cover;
-    background-position: center;
-    text-align: center;
-}
+            @media screen and (max-width: 850px) {
+                font-size: 3rem;
+            }
+        }
+    }
 
-.form .inputBox {
-    margin-bottom: 40px;
-}
+    .about {
+        position: relative;
+        right: 20%;
+    }
 
-.form .inputBox input {
-    width: 30%;
-    background: transparent;
-    box-shadow: none;
-    border: none;
-    outline: none;
-    padding: 10px 0;
-    font-size: 18px;
-    font-weight: 300;
-    color: #fff;
-    border-bottom: 2px solid #fff;
-}
+    .contactContainer h2 {
+        padding: 20px;
+        font-size: 2rem;
+    }
 
-.form .inputBox input::placeholder {
-    color: #fff;
-}
+    .contactContainer .contact {
+        padding: 30px;
+        font-size: 1rem;
+    }
 
-.form .inputBox textarea {
-    width: 30%; /* @TODO - more for mobile and move all to scss. Upgrade backgrounds for mobile... */
-    background: transparent;
-    box-shadow: none;
-    border: none;
-    outline: none;
-    padding: 10px 0;
-    font-size: 18px;
-    font-weight: 300;
-    color: #fff;
-    height: 150px;
-    border-bottom: 2px solid #fff;
-    resize: none;
-}
+    .contentBox {
+        background-size: cover;
+        background-position: center;
+        text-align: center;
+    }
 
-.form .inputBox textarea::placeholder {
-    color: #fff;
-}
+    .form .inputBox {
+        margin-bottom: 40px;
+    }
 
-.form .inputBox input[type='submit'] {
-    width: 150px;
-    background: #fff;
-    color: #111;
-    font-weight: 400;
-}
+    .form .inputBox input {
+        width: 30%;
+        background: transparent;
+        box-shadow: none;
+        border: none;
+        outline: none;
+        padding: 10px 0;
+        font-size: 18px;
+        font-weight: 300;
+        color: #fff;
+        border-bottom: 2px solid #fff;
+    }
 
-.sendButton {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-family: consolas;
-}
-.sendButtonHref {
-    position: relative;
-    display: inline-block;
-    padding: 15px 30px;
-    color: #2196f3;
-    text-transform: uppercase;
-    letter-spacing: 4px;
-    text-decoration: none;
-    font-size: 24px;
-    overflow: hidden;
-    transition: 0.2s;
-    cursor: url('../assets/images/custom-cursor-pointer.png'), pointer !important;
-}
+    .form .inputBox input::placeholder {
+        color: #fff;
+    }
 
-.sendButtonHref:hover {
-    color: #000;
-    background: #2196f3;
-    box-shadow: 0 0 10px #596e80, 0 0 40px #2196f3, 0 0 10px #2196f3;
-    transition-delay: 1s;
-}
+    .form textarea {
+        /* @TODO - more for mobile and move all to scss. Upgrade backgrounds for mobile... */
 
-.sendButtonHref span {
-    position: absolute;
-    display: block;
-}
+        resize: none;
+    }
 
-.sendButtonHref span:nth-child(1) {
-    top: 0;
-    left: -100%;
-    width: 100%;
-    height: 2px;
-    background: linear-gradient(90deg, transparent, #2196f3);
-}
+    .form .inputBox textarea::placeholder {
+        color: #fff;
+    }
 
-.sendButtonHref:hover span:nth-child(1) {
-    left: 100%;
-    transition: 1s;
-}
+    .form .inputBox input[type='submit'] {
+        width: 150px;
+        background: #fff;
+        color: #111;
+        font-weight: 400;
+    }
 
-.sendButtonHref span:nth-child(2) {
-    top: -100%;
-    right: 0;
-    width: 2px;
-    height: 100%;
-    background: linear-gradient(180deg, transparent, #2196f3);
-}
+    :focus {
+        outline: none;
+    }
 
-.sendButtonHref:hover span:nth-child(2) {
-    top: 100%;
-    transition: 1s;
-    transition-delay: 0.25s;
-}
+    .sendButton {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-family: consolas;
+    }
+    .sendButtonHref {
+        position: relative;
+        display: inline-block;
+        padding: 15px 30px;
+        color: #2196f3;
+        text-transform: uppercase;
+        letter-spacing: 4px;
+        text-decoration: none;
+        font-size: 24px;
+        overflow: hidden;
+        transition: 0.2s;
+        cursor: url('../assets/images/custom-cursor-pointer.png'), pointer !important;
+    }
 
-.sendButtonHref span:nth-child(3) {
-    bottom: 0;
-    right: -100%;
-    width: 100%;
-    height: 2px;
-    background: linear-gradient(270deg, transparent, #2196f3);
-}
+    .sendButtonHref:hover {
+        color: #000;
+        background: #2196f3;
+        box-shadow: 0 0 10px #596e80, 0 0 40px #2196f3, 0 0 10px #2196f3;
+        transition-delay: 1s;
+    }
 
-.sendButtonHref:hover span:nth-child(3) {
-    right: 100%;
-    transition: 1s;
-    transition-delay: 0.5s;
-}
+    .sendButtonHref span {
+        position: absolute;
+        display: block;
+    }
 
-.sendButtonHref span:nth-child(4) {
-    bottom: -100%;
-    left: 0;
-    width: 2px;
-    height: 100%;
-    background: linear-gradient(360deg, transparent, #2196f3);
-}
+    .sendButtonHref span:nth-child(1) {
+        top: 0;
+        left: -100%;
+        width: 100%;
+        height: 2px;
+        background: linear-gradient(90deg, transparent, #2196f3);
+    }
 
-.sendButtonHref:hover span:nth-child(4) {
-    bottom: 100%;
-    transition: 1s;
-    transition-delay: 0.75s;
+    .sendButtonHref:hover span:nth-child(1) {
+        left: 100%;
+        transition: 1s;
+    }
+
+    .sendButtonHref span:nth-child(2) {
+        top: -100%;
+        right: 0;
+        width: 2px;
+        height: 100%;
+        background: linear-gradient(180deg, transparent, #2196f3);
+    }
+
+    .sendButtonHref:hover span:nth-child(2) {
+        top: 100%;
+        transition: 1s;
+        transition-delay: 0.25s;
+    }
+
+    .sendButtonHref span:nth-child(3) {
+        bottom: 0;
+        right: -100%;
+        width: 100%;
+        height: 2px;
+        background: linear-gradient(270deg, transparent, #2196f3);
+    }
+
+    .sendButtonHref:hover span:nth-child(3) {
+        right: 100%;
+        transition: 1s;
+        transition-delay: 0.5s;
+    }
+
+    .sendButtonHref span:nth-child(4) {
+        bottom: -100%;
+        left: 0;
+        width: 2px;
+        height: 100%;
+        background: linear-gradient(360deg, transparent, #2196f3);
+    }
+
+    .sendButtonHref:hover span:nth-child(4) {
+        bottom: 100%;
+        transition: 1s;
+        transition-delay: 0.75s;
+    }
+
+    .inner {
+        max-width: 758px;
+        margin: auto;
+        background: #39459b;
+        border: 10px solid #0d99d7;
+        padding: 77px 99px 87px;
+        box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.2);
+        -webkit-box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.2);
+        -moz-box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.2);
+        -ms-box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.2);
+        -o-box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.2);
+    }
+
+    p {
+        text-align: center;
+        padding: 0 10px;
+        margin-bottom: 55px;
+        line-height: 1.8;
+    }
+    .formGroup {
+        position: relative;
+        display: block;
+        margin-bottom: 48px;
+
+        span {
+            font-size: 15px;
+            color: #00ade6;
+            position: absolute;
+            left: 31%;
+            top: 11px;
+            transition: all 0.2s ease;
+            transform-origin: 0 0;
+            cursor: text; //TODO CUSTOM CURSOR
+
+            @media screen and (max-width: 550px) {
+                left: 10%;
+            }
+        }
+        span.border {
+            height: 2px;
+            display: block;
+            position: relative;
+            width: 40%;
+            margin: auto;
+            left: 0;
+            top: 0px;
+            transform: scaleX(0);
+            transition: all 0.15s ease;
+        }
+    }
+    .formControl {
+        border: none;
+        border-bottom: 2px solid #00ade6;
+        display: block;
+        width: 40%;
+        margin: auto;
+        height: 43px;
+        font-size: 15px;
+        background: none;
+        font-family: 'Montserrat-SemiBold';
+        &:focus,
+        &:valid {
+            border: none;
+            border-bottom: 2px solid #fff;
+            & + span {
+                transform: translateY(-22px) scale(0.8);
+                & + .border {
+                    transform: scaleX(1);
+                }
+            }
+        }
+
+        @media screen and (max-width: 550px) {
+            width: 90%;
+        }
+    }
+
+    input,
+    textarea,
+    select,
+    button {
+        font-family: 'Montserrat-Regular';
+        color: #fff;
+        font-size: 15px;
+    }
+
+    textarea.formControl {
+        padding-top: 10px;
+        padding-bottom: 10px;
+    }
 }
 </style>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -5,17 +5,12 @@
         ></h1>
         <div class="projectsSection">
             <Project
-                :logoLink="'weloan.png'"
-                :title="'Weloan'"
-                :content="weloanContent"
-                :projectLink="'https://weloan-app.prpl.io'"
-            />
-
-            <Project
-                :logoLink="'israelbody.png'"
-                :title="'Israelbody'"
-                :content="israelbodyContent"
-                :projectLink="'https://www.israelbody.org'"
+                v-for="project in commercialProjects"
+                :key="project.projectlink"
+                :logoLink="project.logoLink"
+                :title="project.title"
+                :content="project.content"
+                :projectLink="project.projectLink"
             />
         </div>
         <h1>
@@ -23,30 +18,12 @@
         </h1>
         <div class="projectsSection">
             <Project
-                :logoLink="'store.png'"
-                :title="'Store'"
-                :content="storeContent"
-                :projectLink="'https://vezil-store.netlify.app'"
-            />
-            <Project
-                :logoLink="'chat.png'"
-                :title="'Chat'"
-                :content="chatContent"
-                :projectLink="'https://vezil-nodechat.netlify.app'"
-            />
-            <Project
-                :logoLink="'disney.png'"
-                :title="'Disney Plus'"
-                :content="disneyContent"
-                :projectLink="'https://vezil-disneyplus.netlify.app'"
-            />
-            <Project
-                :logoLink="'platformPig.png'"
-                :title="'Platform Pig'"
-                :content="platformPigContent"
-                :projectLink="
-                    'https://vezil.github.io/PlatformPig/PlatformPig.html?07'
-                "
+                v-for="project in myProjects"
+                :key="project.projectlink"
+                :logoLink="project.logoLink"
+                :title="project.title"
+                :content="project.content"
+                :projectLink="project.projectLink"
             />
         </div>
     </div>
@@ -61,30 +38,79 @@ export default {
 
     data() {
         return {
-            weloanContent: `<br/>Project created mainly in Nuxt/Vue/Node(express).
-            This website is using for taking loans (private or business)<br/><br/>
-             <span style="cursor:no-drop; color:gray; padding:5px;">Source Code</span>`,
+            commercialProjects: [
+                {
+                    logoLink: 'weloan.png',
+                    content: `<br/>Project created mainly in Nuxt/Vue/Node(express). 
+                    This website is using for taking loans (private or business)<br/><br/>
+                    <span style="cursor:no-drop; color:gray; padding:5px;">Source Code</span>`,
+                    title: 'Weloan',
+                    projectLink: 'https://weloan-app.prpl.io'
+                },
+                {
+                    logoLink: 'israelbody.png',
+                    content: `<br/>Project created mainly in Nuxt/Vue/Node(express). There is also
+                    Elasticsearch for searching content and Tranzilla system for payment.
+                    In this website we can find a large store with many products and a system 
+                    of coupons and discounts,
+                    food recipes and many others things...<br/><br/>
+                    <span style="cursor:no-drop; color:gray; padding:5px;">Source Code</span>
+                     `,
+                    title: 'Weloan',
+                    projectLink: 'https://www.israelbody.org'
+                },
+                {
+                    logoLink: 'order.png',
+                    content: `<br/>Project created mainly in Vue/Node(hapi). On this page is a lot
+                    of integrations with the POS system (like orderlord). We can buy food via pickup,
+                    delivery or order direct to the table.<br/><br/>
+                    <span style="cursor:no-drop; color:gray; padding:5px;">Source Code</span>
+                     `,
+                    title: 'Order',
+                    projectLink: 'https://order.storekit.com/quarantinos/menu'
+                }
+            ],
 
-            israelbodyContent: `<br/>Project created mainly in Nuxt/Vue/Node(express).There is also
-             Elasticsearch for searching content and Tranzilla system for payment.
-              In this website we can find a large store with many products and a system of coupons and discounts,
-               food recipes and many others things...<br/><br/>
-               <span style="cursor:no-drop; color:gray; padding:5px;">Source Code</span>
-            `,
-
-            storeContent: `<br/>Small shop created in react.js .We can make payment via Paypal<br/><br/>
-            <a style="text-decoration:none; color:gold; padding:5px;" href="https://github.com/Vezil/Store" target="_blank">Source Code</a>`,
-
-            chatContent: `<br/>This chat was created in react.js and node.js + socket.io.
-             You can find here some funcionality like emojify.<br/><br/>
-               <a style="text-decoration:none; color:gold; padding:5px;" href="https://github.com/Vezil/NodeChat" target="_blank">Source Code</a>`,
-
-            disneyContent: `<br/>This project is reproduce disney plus website (created in vue.js)<br/><br/>
-            <a style="text-decoration:none; color:gold; padding:5px;" href="https://github.com/Vezil/disneyplus" target="_blank">Source Code</a>`,
-
-            platformPigContent: `<br/>Experimental Platform Game created in Javascript.<br/><br/> 
-                 <a style="text-decoration:none; color:gold; padding:5px;" href="https://github.com/Vezil/PlatformPig" target="_blank">Source Code</a>
-                `
+            myProjects: [
+                {
+                    logoLink: 'store.png',
+                    content: `<br/>Small shop created in react.js .We can make payment
+                    via Paypal<br/><br/>
+                    <a style="text-decoration:none; color:gold; padding:5px;" 
+                    href="https://github.com/Vezil/Store" target="_blank">Source Code</a>`,
+                    title: 'Store',
+                    projectLink: 'https://vezil-store.netlify.app'
+                },
+                {
+                    logoLink: 'chat.png',
+                    content: `<br/>This chat was created in react.js and node.js + socket.io.
+                    You can find here some funcionality like emojify.<br/><br/>
+                    <a style="text-decoration:none; color:gold; padding:5px;" 
+                    href="https://github.com/Vezil/NodeChat" target="_blank">Source Code</a>`,
+                    title: 'Chat',
+                    projectLink: 'https://vezil-nodechat.netlify.app'
+                },
+                {
+                    logoLink: 'disney.png',
+                    content: `<br/>This project is reproduce disney plus website
+                    (created in vue.js)<br/><br/>
+                    <a style="text-decoration:none; color:gold; padding:5px;" 
+                    href="https://github.com/Vezil/disneyplus" target="_blank">Source Code</a>`,
+                    title: 'Disney Plus',
+                    projectLink: 'https://vezil-disneyplus.netlify.app'
+                },
+                {
+                    logoLink: 'platformPig.png',
+                    content: `<br/>Experimental Platform Game created
+                    in Javascript.<br/><br/> 
+                    <a style="text-decoration:none; color:gold; padding:5px;"
+                    href="https://github.com/Vezil/PlatformPig" target="_blank">Source Code</a>
+                `,
+                    title: 'Platform Pig',
+                    projectLink:
+                        'https://vezil.github.io/PlatformPig/PlatformPig.html?07'
+                }
+            ]
         };
     }
 };
