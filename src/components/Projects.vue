90060060496190ee351a2d8ce2082fbae814ae6e
Szymon Wojaczek <44679435+Vezil@users.noreply.github.com>
2022-07-18 20:21:42 +0200

    feature/add-new-project (#21)
    * add 2048 project
    
    * add hover effect on projectLink class

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="projectsContainer" id="projectsSection">
         <h1>
-            <u>Commercial Projects <i class="fa fa-book"></i></u
-        ></h1>
+            <u>Commercial Projects <i class="fa fa-book"></i></u>
+        </h1>
         <div class="projectsSection">
             <Project
                 v-for="project in commercialProjects"
@@ -77,7 +77,7 @@ export default {
                     logoLink: 'store.png',
                     content: `<br/>Small shop created in react.js .We can make payment
                     via Paypal<br/><br/>
-                    <a style="text-decoration:none; color:gold; padding:5px;" 
+                    <a class="projectLink" 
                     href="https://github.com/Vezil/Store" target="_blank">Source Code</a>`,
                     title: 'Store',
                     projectLink: 'https://vezil-store.netlify.app'
@@ -86,7 +86,7 @@ export default {
                     logoLink: 'chat.png',
                     content: `<br/>This chat was created in react.js and node.js + socket.io.
                     You can find here some funcionality like emojify.<br/><br/>
-                    <a style="text-decoration:none; color:gold; padding:5px;" 
+                    <a class="projectLink"
                     href="https://github.com/Vezil/NodeChat" target="_blank">Source Code</a>`,
                     title: 'Chat',
                     projectLink: 'https://vezil-nodechat.netlify.app'
@@ -95,7 +95,7 @@ export default {
                     logoLink: 'disney.png',
                     content: `<br/>This project is reproduce disney plus website
                     (created in vue.js)<br/><br/>
-                    <a style="text-decoration:none; color:gold; padding:5px;" 
+                    <a class="projectLink" 
                     href="https://github.com/Vezil/disneyplus" target="_blank">Source Code</a>`,
                     title: 'Disney Plus',
                     projectLink: 'https://vezil-disneyplus.netlify.app'
@@ -104,7 +104,7 @@ export default {
                     logoLink: 'platformPig.png',
                     content: `<br/>Experimental Platform Game created
                     in Javascript.<br/><br/> 
-                    <a style="text-decoration:none; color:gold; padding:5px;"
+                    <a class="projectLink"
                     href="https://github.com/Vezil/PlatformPig" target="_blank">Source Code</a>
                 `,
                     title: 'Platform Pig',
@@ -115,14 +115,23 @@ export default {
                     logoLink: 'erp-system.png',
                     content: `<br/>Project created mainly in Vue/Node(express)/Sequelize/MySql. In this project, there are two roles.
                     Admin (Employer) and User (Employee). As a Employee, you can request holidays, and change your password. As an Employer
-                    you can add Employees, add contracts, accept or deny vacations and reset password. Test credentials for a user : <b>user@erp.test password</b>
-                    Test credentials for an admin : <b>admin@erp.test password</b>
-                       <a style="text-decoration:none; color:gold; padding:5px;"
+                    you can add Employees, add contracts, accept or deny vacations and reset password. Test credentials for a user and admin are available on github project description :)<br/>
+                    <a class="projectLink"
                     href="https://github.com/Vezil/ERP-Employee-server" target="_blank">Source Code (backend)</a><br/>
-                    <a style="text-decoration:none; color:gold; padding:5px;" href="https://github.com/Vezil/ERP-Employee-frontend" target="_blank">Source Code (frontend)</a>
+                    <a class="projectLink" href="https://github.com/Vezil/ERP-Employee-frontend" target="_blank">Source Code (frontend)</a>
                      `,
                     title: 'ERP System',
                     projectLink: 'https://vezil-erp-system.netlify.app'
+                },
+                {
+                    logoLink: '2048-game.png',
+                    content: `<br/>A project based on the game "2048". Project created with javascript + p5.js library,
+                     there are also some unit tests. Special thanks for the Coding Traing channel for inspiration :)<br/><br/>
+                    <a class="projectLink"
+                    href="https://github.com/Vezil/2048-game" target="_blank">Source Code</a><br/>
+                     `,
+                    title: '2048 Game',
+                    projectLink: 'https://vezil.github.io/2048-game'
                 }
             ]
         };
