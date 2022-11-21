6c0f397c537e7c36ef3d3b9e3fd0620997bd5942
Vezil <szymonwojaczek@wp.pl>
2022-11-21 19:49:31 +0100

    component init

---
+++ src/components/PageNotFound.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="page-not-found">
+        <div class="text-wrapper">
+            <span class="title">Oya, are you lost?</span>
+
+            <span class="redirect" @click="redirectToLandingPage">
+                Go back to the main page
+            </span>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    methods: {
+        redirectToLandingPage() {
+            window.location.pathname = '';
+        }
+    }
+};
+</script>
